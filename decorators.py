ac6c67c7099d82ebe66a468afd9f637b26e6d1be
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-27 20:51:25 -0700

    modifying decorators to add classes instead of functions

--- decorators.py
+++ decorators.py
@@ -1,10 +1,34 @@
-def outer_func():
-    message = 'Hi'
+# let's review python decorators
+def decorator_function(origional_function):
+    def wrapper_function(*args, **kwargs):
+        print('wrapper exectued this before {}'.format(origional_function.__name__))
+        return origional_function(*args, **kwargs)
+    return wrapper_function
 
-    def inner_func():
-        print(message)
 
-    return inner_func()
+class decorator_class(object):
+    def __init__(self, origional_function):
+        # this ties our functino with an instance of this decorator_class
+        self.origional_function = origional_function
+
+    def __call__(self, *args, **kwargs):
+        print('call exectued this before {}'.format(self.origional_function.__name__))
+        return self.origional_function(*args, **kwargs)
 
 
-outer_func()
+@decorator_class
+def display():
+    print('display function ran')
+
+# we dont need the two lines below if we add the syntax of @decorator_function
+# decorated_display = decorator_function(display)
+
+
+@decorator_class
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
+
+
+display_info('Aaron', 34)
+
+display()
